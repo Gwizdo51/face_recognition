4775e70f3654d2a9f0eac7abdbcb3ac74c804d8b
Gwizdo51 <arthur.clement.51@gmail.com>
2021-07-28 15:15:59 +0200

    reorganised for access

---
+++ test_access_deepface.py
@@ -0,0 +1,12 @@
+import os
+import sys
+# import 
+
+# sys.path.insert(1, '/path/to/application/app/folder')
+
+# from deepface import DeepFace
+
+
+if __name__ == "__main__":
+    # sys.path.insert(1, '/media/arthur/DATA/Code/projects/16_facial_recognition/face_recognition_project_simplon/deepface')
+    print(sys.path)
