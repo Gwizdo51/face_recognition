af59fbabd9bef85ee6374474012f310ea1fe02d4
Gwizdo51 <arthur.clement.51@gmail.com>
2021-08-02 20:22:45 +0200

    renamed django project face_analyzer_server

--- face_analyzer_django/urls.py
+++ face_analyzer_django/urls.py
@@ -1,4 +1,4 @@
-"""face_recognition_server URL Configuration
+"""face_analyzer_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
