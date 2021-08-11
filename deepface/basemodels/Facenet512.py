20142d978cae3da42f2f67f495b6e853f9585c89
Gwizdo51 <arthur.clement.51@gmail.com>
2021-08-11 22:40:12 +0200

    changed spaces to tabs in Facenet512.py

--- deepface/basemodels/Facenet512.py
+++ deepface/basemodels/Facenet512.py
@@ -5,22 +5,22 @@ import gdown
 
 def loadModel(url = 'https://github.com/serengil/deepface_models/releases/download/v1.0/facenet512_weights.h5'):
 
-    model = Facenet.InceptionResNetV2(dimension = 512)
+	model = Facenet.InceptionResNetV2(dimension = 512)
 
-    #-------------------------
+	#-------------------------
 
-    home = str(Path.home())
+	home = str(Path.home())
 
-    if os.path.isfile(home+'/.deepface/weights/facenet512_weights.h5') != True:
-        print("facenet512_weights.h5 will be downloaded...")
+	if os.path.isfile(home+'/.deepface/weights/facenet512_weights.h5') != True:
+		print("facenet512_weights.h5 will be downloaded...")
 
-        output = home+'/.deepface/weights/facenet512_weights.h5'
-        gdown.download(url, output, quiet=False)
+		output = home+'/.deepface/weights/facenet512_weights.h5'
+		gdown.download(url, output, quiet=False)
 
-    #-------------------------
+	#-------------------------
 
-    model.load_weights(home+'/.deepface/weights/facenet512_weights.h5')
+	model.load_weights(home+'/.deepface/weights/facenet512_weights.h5')
 
-    #-------------------------
+	#-------------------------
 
-    return model
+	return model
