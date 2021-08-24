d5150823551ee16698c3ecfe667b820683ca593f
Gwizdo51 <arthur.clement.51@gmail.com>
2021-08-24 16:23:02 +0200

    cleaned up

--- face_analyzer/views.py
+++ face_analyzer/views.py
@@ -121,7 +121,7 @@ class DeepFaceWrapper:
             name = df_result.loc[face_index, "name"]
             is_allowed_in = df_result.loc[face_index, "is_allowed_in"]
             if pd.isnull(name):
-                # draw an orange box
+                # draw an orange box with the name "Unknown"
                 analyzed_img = functions.draw_box(analyzed_img, box, ratio=ratio)
             else:
                 if is_allowed_in:
@@ -192,10 +192,8 @@ def last_analyzed_image(request):
 
         # format the DataFrame
         df_result["total_entry_tickets_bought"] = df_result["total_entry_tickets_bought"].astype('int32')
-        df_result["date_of_birth"] = pd.to_datetime(df_result["date_of_birth"])
-        df_result["date_of_birth"] = df_result["date_of_birth"].dt.strftime('%d-%m-%Y')
-        df_result["creation_date"] = pd.to_datetime(df_result["creation_date"])
-        df_result["creation_date"] = df_result["creation_date"].dt.strftime('%d-%m-%Y')
+        df_result["date_of_birth"] = pd.to_datetime(df_result["date_of_birth"]).dt.strftime('%d %b %Y')
+        df_result["creation_date"] = pd.to_datetime(df_result["creation_date"]).dt.strftime('%d %b %Y')
 
         last_analyzed_img_url = settings.MEDIA_URL + "analyzed_images/" + img_name
         context = {
@@ -208,90 +206,3 @@ def last_analyzed_image(request):
         context = {"db_is_empty": True}
 
     return render(request, "home/show_last_analyzed_image.html", context)
-
-
-# class Analyzer(View):
-
-#     # 'VGG-Face', VGG-Face', 'OpenFace', 'Facenet', 'Facenet512', 'DeepFace', 'DeepID', 'Dlib', 'ArcFace'
-#     # ('Emotion', 'Age', 'Gender', 'Race') not implemented
-#     model_name = 'Facenet'
-#     recog_model = DeepFace.build_model('Facenet')
-#     # 'opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface'
-#     detector = 'mtcnn'
-#     representations = DeepFace.load_representations(
-#         db_path=str(settings.BASE_DIR / "database"),
-#         model_name=model_name,
-#         model=recog_model,
-#         detector_backend=detector,
-#         verbose=True
-#     )
-
-#     # print((settings.BASE_DIR / "database").is_dir())
-#     # print(settings.BASE_DIR)
-#     # print(Path.cwd())
-
-
-#     def get(self, request):
-
-#         form = forms.ImageForm()
-#         return render(request, 'home/index.html', {'form': form})
-
-
-#     def post(self, request):
-
-#         form = forms.ImageForm(request.POST, request.FILES)
-
-#         if form.is_valid():
-
-#             form.save()
-
-#             image_db = models.UploadedImages.objects.latest('id')
-#             # print("#######################################")
-#             # print(repr(Path(image_db.uploaded_img.path).name))
-#             name = Path(image_db.uploaded_img.path).name
-#             image_db.img_name = name
-#             # image_db.save()
-#             # return HttpResponse('tests')
-
-#             # load the image with cv2
-#             # print(images.uploaded_img.url)
-#             # cv2_img = cv2.imread("./" + images.uploaded_img.url)
-
-#             # print(cv2_img.shape)
-#             # cv2.imshow('uploaded_img', cv2_img)
-#             # cv2.waitKey(0)
-#             # cv2.destroyAllWindows()
-
-#             # analyze the image with find_faces
-#             df, analyzed_img = DeepFace.find_faces(
-#                 img_path=str(Path(image_db.uploaded_img.path)),
-#                 db_path=str(settings.BASE_DIR / "database"),
-#                 model_name=self.model_name,
-#                 model=self.recog_model,
-#                 detector_backend=self.detector,
-#                 representations=self.representations,
-#                 verbose=True
-#             )
-#             print(df)
-
-#             # cv2.imshow('analyzed_img', analyzed_img)
-#             # cv2.waitKey(0)
-#             # cv2.destroyAllWindows()
-
-#             # save the analyzed image to media/analyzed_images
-#             analysed_images_dir_path = Path(settings.MEDIA_ROOT / "analyzed_images")
-#             # print(str(analysed_images_dir_path))
-#             if not Path(settings.MEDIA_ROOT / "analyzed_images").is_dir():
-#                 print("no analyzed_images directory yet in media, creating")
-#                 os.mkdir(path=Path(settings.MEDIA_ROOT / "analyzed_images"))
-#             cv2.imwrite(str(analysed_images_dir_path / name), analyzed_img)
-
-#             image_db.save()
-#             # return HttpResponse('tests')
-
-#             # print(settings.MEDIA_URL + "analyzed_images/" + name)
-#             analyzed_img_url = settings.MEDIA_URL + "analyzed_images/" + name
-
-#             # analyzed_img_url = str(Path(images.uploaded_img.url).parent.parent / "analyzed_images" / images.img_name)
-#             context = {"analyzed_img_url": analyzed_img_url}
-#             return render(request, "home/show_analyzed_image.html", context)
